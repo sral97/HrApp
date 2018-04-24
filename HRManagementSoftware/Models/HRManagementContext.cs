249f1b84e0d344bf3a172b681ef525927e894c34
Lars Müller <lars.c.mueller@gmail.com>
2018-04-24 16:42:44 +0200

    Add database context

---
+++ HRManagementSoftware/Models/HRManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagementSoftware.Models
+{
+    public class HRManagementContext : DbContext
+    {
+        public HRManagementContext(DbContextOptions<HRManagementContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
