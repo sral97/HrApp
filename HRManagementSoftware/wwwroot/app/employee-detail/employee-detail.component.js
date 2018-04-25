a49a027d3a627ff912d32201e7f417b140169dca
Lars Müller <lars.c.mueller@gmail.com>
2018-04-25 21:05:28 +0200

    Add create functionality to angular app

--- HRManagementSoftware/wwwroot/app/employee-detail/employee-detail.component.js
+++ HRManagementSoftware/wwwroot/app/employee-detail/employee-detail.component.js
@@ -2,11 +2,20 @@
     module('employeeDetail').
     component('employeeDetail', {
         templateUrl: '/app/employee-detail/employee-detail.template.html',
-        controller: ['$routeParams', '$http', function EmployeeListController($routeParams, $http) {
-            var self = this;
-
-            $http.get('/api/employees/' + $routeParams.employeeId).then(function (response) {
-                self.employee = response.data;
-            });
+        controller: ['$routeParams', '$scope', '$http', '$location', function EmployeeListController($routeParams, $scope, $http, $location) {
+            $scope.save = function (employee) {
+                //$scope.master = angular.copy(employee);
+                $http.post('/api/employees', JSON.stringify(employee)).then(function (response) {
+                    if (response.data.id) {
+                        $scope.employee = response.data;
+                        $location.path('/employees/' + $scope.employee.id);
+                    }
+                });
+            };
+            if ($routeParams.employeeId != "create") {
+                $http.get('/api/employees/' + $routeParams.employeeId).then(function (response) {
+                    $scope.employee = response.data;
+                });
+            }
         }]
     });
