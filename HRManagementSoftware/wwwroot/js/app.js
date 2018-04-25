5980f602629dcd0bfaa42b19227ae28729627b2c
Lars Müller <lars.c.mueller@gmail.com>
2018-04-25 17:42:52 +0200

    Add employee mock data

---
+++ HRManagementSoftware/wwwroot/js/app.js
@@ -0,0 +1,15 @@
+﻿var hrApp = angular.module('hrApp', []);
+
+hrApp.controller('EmployeeController', function EmployeeController($scope) {
+    $scope.employees = [
+        {
+            firstName: 'Lars',
+            lastName: 'Müller',
+            age: 20
+        }, {
+            firstName: 'Max',
+            lastName: 'Mustermann',
+            age: 15
+        }
+    ];
+})
